f697d524bff8f0ccc5c61069978d52fe40b4a42a
LukynkaCZE <jiraklukas058@seznam.cz>
2024-05-14 20:21:55 +0200

    java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,8 +32,13 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
 }
 
 application {
