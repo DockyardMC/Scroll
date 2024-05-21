22bc6349730e5c897421a3c328aaa6fcdc372a3a
LukynkaCZE <48604271+LukynkaCZE@users.noreply.github.com>
2024-05-21 08:27:08 +0200

    a

--- src/main/kotlin/io/github/dockyardmc/scroll/Main.kt
+++ src/main/kotlin/io/github/dockyardmc/scroll/Main.kt
@@ -3,7 +3,4 @@ package io.github.dockyardmc.scroll
 import io.github.dockyardmc.scroll.extensions.toComponent
 
 fun main() {
-    val comp = "<yellow>A Minecraft <rainbow><u>Component Library<yellow> for the <#3d91ff><bold>DockyardMC<yellow> project!".toComponent()
-    println("/tellraw @a ${comp.toJson()}")
 }
-
